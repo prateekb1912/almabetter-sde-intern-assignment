2e6ce0d7dcdb416721b226a7a909dbf741ac9bf0
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-05 20:16:17 +0000

    refine post model

--- app/posts/models.py
+++ app/posts/models.py
@@ -7,13 +7,3 @@ class Post(db.Model):
     message = db.Column(db.String(255), nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
-
-    @property
-    def location(self):
-        return f'({self.latitude},{self.longitude})'
-
-    @location.setter
-    def location(self, point):
-        point = point.strip('()').split(',')
-        self.latitude = float(point[0])
-        self.longitude = float(point[1])
