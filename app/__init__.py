9fd6dde889769b481b34b8e4b7730e9742588bd9
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-07 19:53:40 +0000

    seperate db variables to .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
+import os
+
 from flasgger import Swagger
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+POSTGRES_USER = os.environ.get('POSTGRES_USER', 'user')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'password')
+POSTGRES_DATABASE = os.environ.get('POSTGRES_DATABASE', 'database')
+
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:password@localhost:5432/weathertweets'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DATABASE}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
